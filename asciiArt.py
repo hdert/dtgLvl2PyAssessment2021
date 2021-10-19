026c1b273640461c2186f15b92f687c986a2f576
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2021-10-19 20:11:46 +1300

    create asciiArt.py to store ascii art used in the game.

---
+++ asciiArt.py
@@ -0,0 +1,18 @@
+"""Contains various ascii art used in the program."""
+hangman = [
+    """
+  +—————————————
+  | /          |
+  |/          (_)
+  |            |
+  |           /|\\
+  |            |
+  |\\          / \\
+  | \\
+  +=============
+  """
+]
+
+if __name__ == "__main__":
+    for i in hangman:
+        print(i)
