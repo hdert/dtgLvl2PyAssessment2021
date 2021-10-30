8c8d2bcf4e36f224e7c62ca0211627024280d70e
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2021-10-31 12:24:23 +1300

    Fix string length in valid_uesr_inputs

--- get_name1.py
+++ get_name1.py
@@ -25,7 +25,20 @@ if __name__ == '__main__':
     valid_user_inputs = [
         "John", "John Doe", 1, 9, 999, "1297498", "☭", "J", "Joe D'Angelo",
         "Wade McMcFace",
-        "Adolph Blaine Charles David Earl Frederick Gerald Hubert Irvin John Kenneth Lloyd Martin Nero Oliver Paul Quincy Randolph Sherman Thomas Uncas Victor William Xerxes Yancy Zeus Wolfeschlegel­steinhausen­bergerdorff­welche­vor­altern­waren­gewissenhaft­schafers­wessen­schafe­waren­wohl­gepflege­und­sorgfaltigkeit­beschutzen­vor­angreifen­durch­ihr­raubgierig­feinde­welche­vor­altern­zwolfhundert­tausend­jahres­voran­die­erscheinen­von­der­erste­erdemensch­der­raumschiff­genacht­mit­tungstein­und­sieben­iridium­elektrisch­motors­gebrauch­licht­als­sein­ursprung­von­kraft­gestart­sein­lange­fahrt­hinzwischen­sternartig­raum­auf­der­suchen­nachbarschaft­der­stern­welche­gehabt­bewohnbar­planeten­kreise­drehen­sich­und­wohin­der­neue­rasse­von­verstandig­menschlichkeit­konnte­fortpflanzen­und­sich­erfreuen­an­lebenslanglich­freude­und­ruhe­mit­nicht­ein­furcht­vor­angreifen­vor­anderer­intelligent­geschopfs­von­hinzwischen­sternartig­raum Sr."
+        "Adolph Blaine Charles David Earl Frederick Gerald Hubert Irvin John"
+        "Kenneth Lloyd Martin Nero Oliver Paul Quincy Randolph Sherman Thomas"
+        "Uncas Victor William Xerxes Yancy Zeus Wolfeschlegel­steinhausen­berg"
+        "erdorff­welche­vor­altern­waren­gewissenhaft­schafers­wessen­schafe­w"
+        "aren­wohl­gepflege­und­sorgfaltigkeit­beschutzen­vor­angreifen­durch­"
+        "ihr­raubgierig­feinde­welche­vor­altern­zwolfhundert­tausend­jahres­v"
+        "oran­die­erscheinen­von­der­erste­erdemensch­der­raumschiff­genacht­m"
+        "it­tungstein­und­sieben­iridium­elektrisch­motors­gebrauch­licht­als­"
+        "sein­ursprung­von­kraft­gestart­sein­lange­fahrt­hinzwischen­sternart"
+        "ig­raum­auf­der­suchen­nachbarschaft­der­stern­welche­gehabt­bewohnba"
+        "r­planeten­kreise­drehen­sich­und­wohin­der­neue­rasse­von­verstandig"
+        "­menschlichkeit­konnte­fortpflanzen­und­sich­erfreuen­an­lebenslangli"
+        "ch­freude­und­ruhe­mit­nicht­ein­furcht­vor­angreifen­vor­anderer­int"
+        "elligent­geschopfs­von­hinzwischen­sternartig­raum Sr."
     ]
     invalid_user_inputs = [
         '', ' ', '     ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
