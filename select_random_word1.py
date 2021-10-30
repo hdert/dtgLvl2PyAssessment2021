b1b6d1529f8ee790a14dacd0299d85c8b3aaa4d0
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2021-10-31 12:28:04 +1300

    type hints, rename test functions

--- select_random_word1.py
+++ select_random_word1.py
@@ -31,24 +31,34 @@ if __name__ == "__main__":
     class SimpleTest(unittest.TestCase):
         """Test that the word is in the list of words."""
 
-        def check_word_in_list(self):
+        def test_word_in_list(self):
+            """Test that the word is in the list of words."""
+            word: Optional[str]
             word, _ = select_random_word()
             self.assertIn(word, testing_list_of_words)
 
-        def check_index_of_word(self):
+        def test_index_of_word(self):
+            """Test that the index of the word is correct."""
+            word: Optional[str]
+            index_of_word: List[int]
             word, index_of_word = select_random_word()
             self.assertEqual(word, testing_list_of_words[index_of_word[-1]])
 
-        def check_index_is_correct(self):
-            false_indexes = sample(range(0, len(list_of_words)), 5)
+        def test_index_is_correct(self):
+            """Check that the indexes given are returned properly."""
+            index_of_word: List[int]
+            false_indexes: List[int]
+            false_indexes = sample(range(0, len(testing_list_of_words)), 5)
             _, index_of_word = select_random_word(false_indexes)
             for i in false_indexes:
                 self.assertIn(i, index_of_word)
 
-        def self_check_lists_are_equal(self):
-            used_words = []
-            words = []
+        def test_check_lists_are_equal(self):
+            """Check that the test list and actual lists are the same."""
+            used_words: Optional[List[int]] = None
+            words: List[str] = []
             while True:
+                word: str
                 word, used_words = select_random_word(used_words)
                 if word is None:
                     break
