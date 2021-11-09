e3d0fc56dea9e2d299cfc930ce12833abdf2a375
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2021-11-10 06:55:39 +1300

    write test for check_user_wants_to_play_again

--- check_user_wants_to_play_again1.py
+++ check_user_wants_to_play_again1.py
@@ -17,4 +17,36 @@ def check_user_wants_to_play_again() -> bool:
 
 
 if __name__ == '__main__':
-    check_user_wants_to_play_again()
+    import unittest
+    from unittest.mock import patch
+    from typing import Union, List
+
+    valid_inputs = ['y', 'n', 'Y', 'N']
+    invalid_inputs: List[Union[None, int, str,
+                               float]] = [None, 1, 1.5, 'a', 'yy', 'nn', 'Yy']
+
+    class SimpleTest(unittest.TestCase):
+        """Test that check_user_wants_to_play_again works."""
+
+        @patch('__main__.input')
+        def test_valid_inputs(self, mocked):
+            """Test that valid_inputs are valid."""
+            for i in valid_inputs:
+                mocked.return_value = i
+                self.assertEqual(check_user_wants_to_play_again(),
+                                 i.lower() == 'y')
+
+        @patch('__main__.input')
+        def test_invalid_inputs(self, mocked):
+            """Test that invalid_inputs are invalid."""
+            for i in invalid_inputs:
+                mocked.side_effect = i, valid_inputs[0]
+                # There are only two types of valid_inputs str.lower() == 'y'
+                # and str.lower() == 'n', therefore if you do an evaluation for
+                # str.lower() == 'y', you replicate the functionality of the
+                # function without the error checking, for testing puposes as
+                # we know this is valid data
+                self.assertEqual(check_user_wants_to_play_again(),
+                                 valid_inputs[0].lower() == 'y')
+
+    unittest.main()
