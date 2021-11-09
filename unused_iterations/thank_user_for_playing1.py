385d7ca1a8341726bea856a251b2f4821cdcafdb
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2021-11-09 17:28:50 +1300

    wrtie thank_user_for_playing

---
+++ unused_iterations/thank_user_for_playing1.py
@@ -0,0 +1,8 @@
+"""Thank the user for playing."""
+
+
+def thank_user_for_playing(score):
+    """Thank the user for playing and display the final score."""
+    print(f"""
+Thank you for playing hangman!
+Your final score is: {score}""")
