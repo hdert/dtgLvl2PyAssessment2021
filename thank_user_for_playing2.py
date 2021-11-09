0114672ddc151178c35183625b8b87b93741dd76
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2021-11-09 17:29:02 +1300

    add user's name to the thank you message

---
+++ thank_user_for_playing2.py
@@ -0,0 +1,8 @@
+"""Thank the user for playing."""
+
+
+def thank_user_for_playing(score, user_name):
+    """Thank the user for playing and display the final score."""
+    print(f"""
+Thank you for playing hangman {user_name}!
+Your final score is: {score}""")
